9ccf577c3cfb6c83fdf3cc85b5c319ed0d547bc7
skarolk <skarolkiewicz@gmail.com>
2018-11-28 17:00:52 -0500

    fixed typos preventing PATCH

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create, :update]
 
   def profile
     render json: { user: UserSerializer.new(current_user) }, status: :accepted
@@ -15,10 +15,20 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :image, :email, :password, :matched, :club_id)
+    params.require(:user).permit(:username, :image, :email, :password, :matched, :club_id, :id)
   end
 
 end
