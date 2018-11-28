bcdc77486765508b1c53d707bbc02daf93b3cb46
skarolk <skarolkiewicz@gmail.com>
2018-11-27 20:35:11 -0500

    auth implementation milestone 2

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::UsersController < ApplicationController
-
-  def index
-    @users = User.all
-    render json: @users
-  end
+  skip_before_action :authorized, only: %i[create]
 
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
