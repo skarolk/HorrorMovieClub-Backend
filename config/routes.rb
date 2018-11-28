53a08ba9b70f9abf91002556574836ab2e357596
skarolk <skarolkiewicz@gmail.com>
2018-11-27 23:06:58 -0500

    small fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
-        resources :users, only: %i[create]
+        resources :users, only: [:create]
         post '/login', to: 'auth#create'
         get '/profile', to: 'users#profile'
         resources :clubs, only: [:index, :create]
