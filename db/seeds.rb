1d2e7ca7eb8264b90dd27fe272662da779dc3576
skarolk <skarolkiewicz@gmail.com>
2018-12-06 11:49:01 -0500

    good seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ end
 def create_movies()
   page_number = 1
   total_pages = page_check()
-  while page_number < 20
+  while page_number < 25
     movies = Tmdb::Genre.movies(27, page: page_number)
     i = 0
     while i < 20
@@ -53,54 +53,56 @@ puts "beging seeding test data"
 
 puts "seeding clubs"
 Club.create(movie_id: 54, active: true)
-Club.create(movie_id: 2, active: true)
-Club.create(movie_id: 3, active: true)
-Club.create(movie_id: 4, active: false)
-Club.create(movie_id: 62, active: true)
+Club.create(movie_id: 47, active: true)
+Club.create(movie_id: 36, active: true)
 
 puts "seeding users"
-User.create(username: "Seb", email: "seb@seb", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Chris", email: "chris@chris", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Sabrina", email: "sabrina@sabrina", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/RTdi6B1.png")
+User.create(username: "SeaBass_1", email: "seb@seb", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/z9IHE6c.jpg")
+User.create(username: "CurseXO", email: "chris@chris", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/15J2mom.jpg")
+User.create(username: "ThunderB0X", email: "sabrina@sabrina", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/K3OkLie.jpg")
 
-User.create(username: "Sasha", email: "sasha@sasha", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "ChiChi", email: "chichi@chichi", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Dude", email: "dude@dude", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/RTdi6B1.png")
+User.create(username: "Organelle", email: "leszek@leszek", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/bOZB0Cr.jpg")
+User.create(username: "MurderPunch", email: "baby@baby", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/dwIweoZ.jpg")
+User.create(username: "DiabloFan", email: "panther@panther", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/capjYyg.jpg")
 
-User.create(username: "Bentley", email: "bentley@bentley", password: "secure", matched: true, club_id: 5, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Spencer", email: "spencer@spencer", password: "secure", matched: true, club_id: 5, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Tom", email: "tom@tom", password: "secure", matched: true, club_id: 5, image: "https://i.imgur.com/RTdi6B1.png")
+User.create(username: "Nightmare", email: "sasha@sasha", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/gqpYNzZ.jpg")
+User.create(username: "ClownFan", email: "chichi@chichi", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/T8vQiZn.jpg")
+User.create(username: "Terrifier", email: "dude@dude", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/wr7hnGt.jpg")
 
-User.create(username: "Bozena", email: "bozena@bozena", password: "secure", matched: false, club_id: 5, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Nicole", email: "nicole@nicole", password: "secure", matched: false, club_id: 5, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Al", email: "al@al", password: "secure", matched: false, club_id: 5, image: "https://i.imgur.com/RTdi6B1.png")
+User.create(username: "ChuckyLover", email: "sasha@sasha", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/6CK3rj2.jpg")
+User.create(username: "MrsVoorhees", email: "chichi@chichi", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/uKL4VNw.jpg")
+User.create(username: "WitchKraft", email: "dude@dude", password: "secure", matched: true, club_id: 2, image: "https://i.imgur.com/BvYLhZ7.jpg")
 
-User.create(username: "Leszek", email: "leszek@leszek", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Baby", email: "baby@baby", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/RTdi6B1.png")
-User.create(username: "Panther", email: "panther@panther", password: "secure", matched: true, club_id: 1, image: "https://i.imgur.com/RTdi6B1.png")
+User.create(username: "HorrorFan", email: "bentley@bentley", password: "secure", matched: true, club_id: 3, image: "https://i.imgur.com/YtgYx95.jpg")
+User.create(username: "80sHorror", email: "spencer@spencer", password: "secure", matched: true, club_id: 3, image: "https://i.imgur.com/SXeS1Sy.jpg")
+User.create(username: "Chiller", email: "tom@tom", password: "secure", matched: true, club_id: 3, image: "https://i.imgur.com/QwLEERZ.jpg")
 
-puts "seeding ratings"
-Rating.create(user_id: 1, liked: true, movie_id: 1)
-Rating.create(user_id: 1, liked: true, movie_id: 2)
-Rating.create(user_id: 2, liked: false, movie_id: 1)
-Rating.create(user_id: 3, liked: false, movie_id: 1)
+User.create(username: "C_Keeper", email: "bozena@bozena", password: "secure", matched: true, club_id: 3, image: "https://i.imgur.com/dwIweoZ.jpg")
+User.create(username: "Annabelle", email: "nicole@nicole", password: "secure", matched: true, club_id: 3, image: "https://i.imgur.com/gqpYNzZ.jpg")
+User.create(username: "CampyHorror", email: "al@al", password: "secure", matched: true, club_id: 3, image: "https://i.imgur.com/wr7hnGt.jpg")
 
-Rating.create(user_id: 4, liked: true, movie_id: 2)
-Rating.create(user_id: 4, liked: true, movie_id: 3)
-Rating.create(user_id: 5, liked: false, movie_id: 2)
-Rating.create(user_id: 6, liked: false, movie_id: 2)
-
-Rating.create(user_id: 7, liked: true, movie_id: 3)
-Rating.create(user_id: 8, liked: false, movie_id: 3)
-Rating.create(user_id: 9, liked: false, movie_id: 3)
-
-Rating.create(user_id: 10, liked: true, movie_id: 4)
-Rating.create(user_id: 11, liked: false, movie_id: 4)
-Rating.create(user_id: 12, liked: false, movie_id: 4)
-
-Rating.create(user_id: 13, liked: true, movie_id: 1)
-Rating.create(user_id: 14, liked: false, movie_id: 1)
-Rating.create(user_id: 15, liked: false, movie_id: 1)
+# puts "seeding ratings"
+# Rating.create(user_id: 1, liked: true, movie_id: 1)
+# Rating.create(user_id: 1, liked: true, movie_id: 2)
+# Rating.create(user_id: 2, liked: false, movie_id: 1)
+# Rating.create(user_id: 3, liked: false, movie_id: 1)
+#
+# Rating.create(user_id: 4, liked: true, movie_id: 2)
+# Rating.create(user_id: 4, liked: true, movie_id: 3)
+# Rating.create(user_id: 5, liked: false, movie_id: 2)
+# Rating.create(user_id: 6, liked: false, movie_id: 2)
+#
+# Rating.create(user_id: 7, liked: true, movie_id: 3)
+# Rating.create(user_id: 8, liked: false, movie_id: 3)
+# Rating.create(user_id: 9, liked: false, movie_id: 3)
+#
+# Rating.create(user_id: 10, liked: true, movie_id: 4)
+# Rating.create(user_id: 11, liked: false, movie_id: 4)
+# Rating.create(user_id: 12, liked: false, movie_id: 4)
+#
+# Rating.create(user_id: 13, liked: true, movie_id: 1)
+# Rating.create(user_id: 14, liked: false, movie_id: 1)
+# Rating.create(user_id: 15, liked: false, movie_id: 1)
 
 puts "seeding finished"
 
